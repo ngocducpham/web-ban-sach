de51accb3013bc752242d62a11a6b0ae07b620b8
TheHien102 <huynhthehien11102001@gmail.com>
2021-11-24 09:31:43 +0700

    Page login, register

---
+++ src/main/java/com/doancntt/controllers/RegisterServlet.java
@@ -0,0 +1,22 @@
+package com.doancntt.controllers;
+
+import com.doancntt.utils.ServletUtils;
+
+import javax.servlet.*;
+import javax.servlet.http.*;
+import javax.servlet.annotation.*;
+import java.io.IOException;
+
+@WebServlet(name = "RegisterServlet", value = "/RegisterServlet")
+public class RegisterServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        ServletUtils.forward("views/registor/register.jsp", request,response);
+
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+}
