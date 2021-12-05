76b207a153b6da0449f9b1e0915c16be12948ad3
ducphamngoc <19110157@student.hcmute.edu.vn>
2021-12-05 21:36:53 +0700

    thêm backend lấy phí ship

---
+++ src/main/java/com/doancntt/controllers/ShipFeeServlet.java
@@ -0,0 +1,56 @@
+package com.doancntt.controllers;
+
+import javax.servlet.*;
+import javax.servlet.http.*;
+import javax.servlet.annotation.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.nio.charset.StandardCharsets;
+
+@WebServlet(name = "ShipFeeServlet", value = "/ShipFeeServlet")
+public class ShipFeeServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String from = request.getParameter("f");
+        String to = request.getParameter("t");
+        String weight = request.getParameter("w");
+
+        String httpRequest = sendHTTPRequest(from, to, weight);
+
+        PrintWriter printWriter = response.getWriter();
+        response.setContentType("text/html");
+        response.setCharacterEncoding("utf-8");
+
+        printWriter.print(httpRequest);
+        printWriter.flush();
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+
+    public static String sendHTTPRequest(String from, String to, String weight){
+        try{
+            URL url = new URL("http://www.vnpost.vn/vi-vn/tra-cuu-gia-cuoc?from=" +from+ "&to="+to+"&weight=" + weight);
+            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+            connection.setRequestMethod("GET");
+            String line = "";
+            InputStreamReader streamReader = new InputStreamReader(connection.getInputStream(), StandardCharsets.UTF_8);
+            BufferedReader bufferedReader = new BufferedReader(streamReader);
+            StringBuilder response = new StringBuilder();
+            while ((line = bufferedReader.readLine()) != null){
+                response.append(line);
+            }
+            bufferedReader.close();
+            return  response.toString();
+        }
+        catch (Exception e){
+            return "";
+        }
+    }
+}
